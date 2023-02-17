4c31597b05a975be190a320783970c6b32c9e815
tanya15gupta <66014813+tanya15gupta@users.noreply.github.com>
2023-02-17 07:11:51 +0530

    Game Won Condition

--- Assets/_project/Scripts/Environment/ObstacleCubeController.cs
+++ Assets/_project/Scripts/Environment/ObstacleCubeController.cs
@@ -9,7 +9,8 @@ namespace Hypercasual
 		[SerializeField] private int m_MinHealth;
 		[SerializeField] private int m_MaxHealth;
 		[SerializeField] private TextMeshPro m_ObstacleText;
-
+		[SerializeField] private m_EnemyTypes m_CurrentEnemyType;
+		private enum m_EnemyTypes {	NORMAL, BOSS }
 		private void Start()
 		{
 			m_ObstacleHealth = (int)Random.Range(m_MinHealth, m_MaxHealth);
@@ -27,9 +28,14 @@ namespace Hypercasual
 		private void ReceivedHit()
 		{
 			m_ObstacleHealth--;
-			if (m_ObstacleHealth <= 0)
+			if (m_ObstacleHealth <= 0 && m_CurrentEnemyType == m_EnemyTypes.NORMAL)
 			{
 				Destroy(gameObject);
+			}
+			if (m_ObstacleHealth <= 0 && m_CurrentEnemyType == m_EnemyTypes.BOSS)
+			{
+				Destroy(gameObject);
+				UIService.Instance.SetGameWonPanelActive(true);
 			}
 			UpdateHealthText();
 		}
